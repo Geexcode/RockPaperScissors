a7b828212259057dfead48d08804f2e83407a695
gibranight17@gmail.com <gibranhub@gmail.com>
2021-09-19 09:39:46 +0700

    finishing app

---
+++ app/src/main/java/com/geeb/rockpaperscissors/Constants.kt
@@ -0,0 +1,14 @@
+package com.geeb.rockpaperscissors
+
+class Constants {
+    companion object {
+        const val ROCK = "rock"
+        const val PAPER = "paper"
+        const val SCISSORS = "scissors"
+        const val DRAW = "DRAW"
+        const val PLAYER = "player"
+        const val PLAYER_WIN = "PLAYER WIN"
+        const val COM = "com"
+        const val COM_WIN = "COM WIN"
+    }
+}
